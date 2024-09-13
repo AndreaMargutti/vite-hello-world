c301333e100f9c30f74da95b6da21810e1eb578f
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-13 16:41:29 +0200

    problem solved - esercizio finito (no bonus)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
-<script setup>
+<script>
 import AppTitle from './components/AppTitle.vue';
 export default {
   components: {
     AppTitle,
-  },
-
-  data() {
-    return {
-      count: 0;
-    }
   }
 }
 </script>
